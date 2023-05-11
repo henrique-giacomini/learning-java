40c7c78615b42c8f903cab57aa18b13391d64c68
henrique-giacomini <hggiacomini@gmail.com>
2023-05-11 20:26:55 -0300

    add comments

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
+    /* Multiple lines comments are just like in SQL
+     * which is good since I've been used to work a lot with database languages
+     */
     public static void main(String[] args) {
         System.out.println("Hello World! I just start to learning Java isn't it nice?");
-    }
+    } // and finally a one line comment
 }
